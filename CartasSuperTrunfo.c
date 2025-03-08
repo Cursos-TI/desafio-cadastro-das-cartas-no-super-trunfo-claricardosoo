d9753d9092cefeb2bf4b9e649dfb90bf501c8517
claricardosoo <202501526624@alunos.estacio.br>
2025-03-08 01:48:42 +0000

    Finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,13 +2,13 @@
 
 int main(){
 
-char estado;
+char estado[10];
 char codigo[20];
 char cidade[50];
 int populacao, turismo;
 float area, pib;
 
-char estado2;
+char estado2[10];
 char codigo2[20];
 char cidade2[50];
 int populacao2, turismo2;
@@ -23,10 +23,10 @@ printf ("Para jogar, cadastre os dados de suas cartas.\n Inicialmente, cada joga
 
 printf ("Primeira carta \n\n");
 printf ("Estado: \n");
-scanf ( "%c",&estado);
+scanf ( "%s", estado);
 
 printf ("Codigo: \n");
-scanf ( "%s",&codigo);
+scanf ( "%s",codigo);
 
 printf ("Cidade: \n");
 scanf ("%s", cidade);
@@ -45,46 +45,46 @@ scanf ("%d", &turismo);
 
 printf ("Cadastre a segunda carta. \n\n");
 printf ("Segunda carta \n\n");
-printf ("Estado: ");
-scanf ("%c", &estado2);
+printf ("Estado: \n");
+scanf ("%s", estado2);
 
-printf ("Codigo: ");
-scanf ("%s" , &codigo2);
+printf ("Codigo: \n");
+scanf ("%s" , codigo2);
 
-printf ("Cidade:");
-scanf (" %[^\n]", cidade2);
+printf ("Cidade: \n");
+scanf (" %s", cidade2);
 
-printf ("Populacao: ");
+printf ("Populacao: \n");
 scanf ("%d", &populacao2);
 
-printf ("Area(km²): ");
-scanf ("%2f", &area2);
+printf ("Area(km²): \n");
+scanf ("%f", &area2);
 
-printf ("PIB: ");
+printf ("PIB: \n");
 scanf ("%f", &pib2);
 
-printf ("Pontos turisticos: ");
-scanf ("%d", &turisticos2);
+printf ("Pontos turisticos: \n");
+scanf ("%d", &turismo2);
 
 printf("\n\nCartas cadastradas com sucesso!\n\n");
 
 printf ("Primeira carta\n\n");
-printf ("Estado: %c \n", estado1);
-printf ("Codigo: %s \n", codigo1);
-printf ("Cidade: %s \n", cidade1);
-printf ("Populacao: %d \n", populacao1);
-printf ("Area(km²): %f \n", area1);
-printf ("PIB: %f \n", pib1);
-printf ("Pontos turisticos: %d \n", turisticos1);
+printf ("Estado: %s \n", estado);
+printf ("Codigo: %s \n", codigo);
+printf ("Cidade: %s \n", cidade);
+printf ("Populacao: %d \n", populacao);
+printf ("Area(km²): %f \n", area);
+printf ("PIB: %f \n", pib);
+printf ("Pontos turisticos: %d \n", turismo);
 
 printf ("Segunda carta\n\n");
-printf ("Estado: %c \n", estado2);
+printf ("Estado: %s \n", estado2);
 printf ("Codigo: %s \n", codigo2);
 printf ("Cidade: %s \n", cidade2);
 printf ("Populacao: %d \n", populacao2);
 printf ("Area(km²): %f \n", area2);
 printf ("PIB: %f \n", pib2);
-printf ("Pontos turisticos: %d \n", turisticos2);
+printf ("Pontos turisticos: %d \n", turismo2);
 
 printf ("Bom jogo e boa sorte");
 
